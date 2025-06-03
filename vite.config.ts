4ba186b16698ffda3ba798844decf0350d061eaf
Ticket2Andromeda <jaybp95@gmail.com>
2025-06-03 00:23:03 -0400

    new deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), svelte()],
+  base: "/turtlebot-visualizer/",
   optimizeDeps: {
     include: ["roslib"],
   },
